6586fcfa0dee87e2b0a2c731190c93062596b1f7
dcalled <davidc980@outlook.com>
2023-03-20 19:38:41 -0500

    Advances

--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,8 @@ const lang = "en_US";
 
 //console.log(await scrapDefaultList(`${sc.baseURL}/${lang}/${sc.champion.list.path}`, sc.champion.list.selector, sc.champion.list.name_tag));
 //console.log(await scrapDefaultList(`${sc.baseURL}/${lang}/${sc.champion.path}/aphelios`, sc.champion.related.selector, sc.champion.related.name_tag));
-console.log(await scrapDefaultList(`${sc.baseURL}/${lang}/${sc.champion.path}/ahri`, sc.champion.stories.selector, sc.champion.stories.name_tag));
+//console.log(await scrapDefaultList(`${sc.baseURL}/${lang}/${sc.champion.path}/ahri`, sc.champion.stories.selector, sc.champion.stories.name_tag));
+
+//console.log(await scrapDefaultList(`${sc.baseURL}/${lang}/${sc.region.list.path}`, sc.region.list.selector, sc.region.list.name_tag));
+//console.log(await scrapDefaultList(`${sc.baseURL}/${lang}/${sc.region.path}/piltover`, sc.region.champs.selector, sc.region.champs.name_tag));
+console.log(await scrapDefaultList(`${sc.baseURL}/${lang}/${sc.region.path}/ionia`, sc.region.stories.selector, sc.region.stories.name_tag, sc.region.stories.scroll));
--- src/scrapper/scanners/ListScrapper.ts
+++ src/scrapper/scanners/ListScrapper.ts
@@ -7,13 +7,42 @@ interface DefaultListItem {
 
 type DefaultList = Array<DefaultListItem>;
 
-export async function scrapDefaultList(url: string, listSelector: string, keyTag: string): Promise<any> {
-    return await scrapPage/*<DefaultList>*/(url, async page => {
+export async function scrapDefaultList(url: string, listSelector: string, keyTag: string, buttonSelector: string | null = null) {
+    return await scrapPage<DefaultList>(url, async page => {
+        let urls: DefaultList = [];
+        for (let i = 0; i<40; i++) {
+            await page.waitForSelector(listSelector);
+            let newUrls = await page.$$eval(listSelector, (links: Element[], tag: string) => {
+                return links.map(el => {
+                    const t = {
+                        url: el.hasAttribute('href') ? (<HTMLAnchorElement>el).href : el.querySelector("a")!.href,
+                        name: Array.from(el.querySelectorAll(tag))!.reduce((prev, curr) => `${prev}${curr.textContent}`, '')
+                    };
+                    return t;
+                })
+            }, keyTag);
+            if(buttonSelector == null) {
+                urls = newUrls;
+                break;
+            }
+            for(let i = 0; i < newUrls.length; ++i) {
+                const e1 = newUrls[i];
+                const haveItem = urls.filter(e2 => e1.name == e2.name && e1.url == e2.url).length > 0;
+                if(!haveItem) urls.push(e1);
+            }
+            await (await page.$(buttonSelector))!.click();
+        }
+        return urls;
+    });
+}
+
+/* export async function scrapDefaultList(url: string, listSelector: string, keyTag: string): Promise<DefaultList> {
+    return await scrapPage<DefaultList>(url, async page => {
         await page.waitForSelector(listSelector);
         let urls = await page.$$eval(listSelector, (links: Element[], tag: string) => {
             return links.map(el => {
-                const t = { 
-                    url: el.querySelector("a")!.href, 
+                const t = {
+                    url: el.hasAttribute('href') ? (<HTMLAnchorElement>el).href : el.querySelector("a")!.href,
                     name: Array.from(el.querySelectorAll(tag))!.reduce((prev, curr) => `${prev}${curr.textContent}`, '')
                 };
                 return t;
@@ -21,6 +50,4 @@ export async function scrapDefaultList(url: string, listSelector: string, keyTag
         }, keyTag);
         return urls;
     });
-}
-
-
+} */
